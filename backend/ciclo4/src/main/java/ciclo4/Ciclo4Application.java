1a19846e78a2ac84a04a0b7d3c682bd7339bbdef
Jcam <kolinin@gmail.com>
2021-11-21 18:18:55 -0500

    application.properties y Main

--- backend/ciclo4/src/main/java/ciclo4/Ciclo4Application.java
+++ backend/ciclo4/src/main/java/ciclo4/Ciclo4Application.java
@@ -2,7 +2,11 @@ package ciclo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
+@EntityScan(basePackages={"ciclo4.modelo"})
 @SpringBootApplication
 public class Ciclo4Application {
 
