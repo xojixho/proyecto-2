d4a778e9156e346b4ddbfb91a11531270cccca2a
Jcam <kolinin@gmail.com>
2021-12-03 19:02:39 -0500

    actualizar, eliminar en usuarios. Cambiar cadena de conexion

--- reto2/backend/Retos_Ciclo4/src/main/resources/static/js/usuarios.js
+++ reto2/backend/Retos_Ciclo4/src/main/resources/static/js/usuarios.js
@@ -8,15 +8,15 @@ function listarUsuarios() {
         type: "GET",
         dataType: "JSON",
         success: function (respuesta) {
-            console.log(respuesta);
             llenarTablaUsuarios(respuesta);
         }
     });
 }
 
 function llenarTablaUsuarios(respuesta) {
-    let tbody = "<tr>";
+    let tbody = "<table>";
     for (i = 0; i < respuesta.length; i++) {
+        tbody+="<tr>";
         tbody += "<th scope='row'>" + respuesta[i].identification + "</th>";
         tbody += "<td>" + respuesta[i].name + "</td>";
         tbody += "<td>" + respuesta[i].address + "</td>";
@@ -27,16 +27,17 @@ function llenarTablaUsuarios(respuesta) {
         tbody += "<td>" + respuesta[i].type + "</td>";
         tbody += "<td><button type='button' class='btn btn-success' onclick='formularioEditarUsuario(" + JSON.stringify(respuesta[i].id) + ")'>Editar</button></td>";
         tbody += "<td><button type='button' class='btn btn-danger' onclick='borrarUsuario(" + JSON.stringify(respuesta[i].id) + ")'>Borrar</button></td>";
+        tbody+="</tr>";
     }
 
-    tbody += "</tr>";
+    tbody += "</table>";
     $("#resultadoUsuarios").html(tbody);
 }
 
 
 function formularioEditarUsuario(usuario){
     $("#nuevoUsuario").hide();
-    $("#titulo").html("editar usuario");
+    $("#titulo").html("Editar usuario");
     $("#contenido").html(
         `
         <div class="col"></div>
@@ -147,6 +148,176 @@ function formularioEditarUsuario(usuario){
 }
 
 
+
+
 function editarUsuario(){
-    console.log('hola');
+    if(checkFields()){
+        updateUser();
+    }
+}
+
+function limpiarCampos() {
+    $("#id").val("");
+    $("#identificacion").val("");
+    $("#nombre").val("");
+    $("#email").val("");
+    $("#clave").val("");
+    $("#telefono").val("");
+    $("#direccion").val("");
+    $("#zona").val("NO DEFINIDO");
+    $("#tipo").val("NO DEFINIDO");
+}
+
+/**
+ * validar string vacia
+ * @param {type} field
+ * @returns {Boolean}
+ */
+ function isEmpty(field) {
+    return field === "";
+}
+
+/**
+ * Validar campos vacios
+ * @returns {Boolean}
+ */
+function checkFields() {
+    let id = $("#id").val();
+    let identification = $("#identificacion").val();
+    let nombre = $("#nombre").val();
+    let direccion = $("#direccion").val();
+    let cellPhone = $("#telefono").val();
+    let email = $("#email").val();
+    let password = $("#clave").val();
+    let zona = $("#zona").val();
+    let tipo = $("#tipo").val();
+
+
+    $("#tituloUsuario").text("");
+    $("#userModal").modal("hide");
+    if (isEmpty(id)) {
+        $("#tituloUsuario").text("Ingresar id");
+        $("#userModal").modal("show");
+        return false;
+    } else if (isEmpty(identification)) {
+        $("#tituloUsuario").text("Ingresar identificación");
+        $("#userModal").modal("show");
+        return false;
+    } else if (isEmpty(nombre)) {
+        $("#tituloUsuario").text("Ingresar nombre");
+        $("#userModal").modal("show");
+        return false;
+    } else if (isEmpty(direccion)) {
+        $("#tituloUsuario").text("Ingresar dirección");
+        $("#userModal").modal("show");
+        return false;
+    } else if (isEmpty(cellPhone)) {
+        $("#tituloUsuario").text("Ingresar teléfono");
+        $("#userModal").modal("show");
+        return false;
+    } else if (isEmpty(email)) {
+        $("#tituloUsuario").text("Ingresar email");
+        $("#userModal").modal("show");
+        return false;
+    } else if (isEmpty(password)) {
+        $("#tituloUsuario").text("Ingresar clave");
+        $("#userModal").modal("show");
+        return false;
+    } else if (zona == "NO DEFINIDO") {
+        $("#tituloUsuario").text("Ingresar zona");
+        $("#userModal").modal("show");
+        return false;
+    } else if (tipo == "NO DEFINIDO") {
+        $("#tituloUsuario").text("Ingresar tipo");
+        $("#userModal").modal("show");
+        return false;
+    } else
+        return true;
+
+}
+
+function updateUser() {
+
+    $("#tituloUsuario").text("");
+    $("#userModal").modal("hide");
+
+    var data = {
+        id: $("#id").val(),
+        identification: $("#identificacion").val(),
+        name: $("#nombre").val(),
+        address: $("#direccion").val(),
+        cellPhone: $("#telefono").val(),
+        email: $("#email").val(),
+        password: $("#clave").val(),
+        zone: $("#zona").val(),
+        type: $("#tipo").val()
+
+    };
+
+    $.ajax({
+        url: "http://localhost:8080/api/user/update",
+        async: true,
+        type: "PUT",
+        dataType: "JSON",
+        contentType: "application/JSON; charset=utf-8",
+        data: JSON.stringify(data),
+
+        success: function (json) {
+            limpiarCampos();
+            $('#tituloUsuario').text('Cambios realizados de forma exitosa');
+            $('#userModal').modal('show');
+            $('#close').hide();
+            irListarUsuarios();
+        },
+        error: function (error) {
+            $('#tituloUsuario').text(`No fue posible editar la cuenta`);
+            $('#userModal').modal('show');
+        }
+    });
+}
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            //resolve(x);
+            window.location = "usuarios.html";
+        }, 500);
+    });
+}
+
+async function irListarUsuarios() {
+    var x = await resolveAfter2Seconds();
+
+    //console.log(x); // 10
+}
+
+
+/**
+ * Metodo eliminar
+ * @param {*} codigo 
+ */
+
+function borrarUsuario(codigo) {
+    let datos = {
+        id: codigo
+    }
+
+    let = datoBorrar = JSON.stringify(datos);
+    $.ajax({
+        url: "http://localhost:8080/api/user/"+codigo,
+        data: datoBorrar,
+        type: "DELETE",
+        contentType: "application/JSON",
+        dataType: "json",
+        success: function (respuesta) {
+            $('#tituloUsuario').text('Usuario eliminado de forma exitosa');
+            $('#userModal').modal('show');
+            $('#close').hide();
+            irListarUsuarios();
+        },
+        error: function (xhr, status) {
+            $('#tituloUsuario').text(`No fue posible eliminar la cuenta`);
+            $('#userModal').modal('show');
+        }
+    });
 }
