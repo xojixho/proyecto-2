1a12380aac9e8da67e285a9023871438b0a57117
Jcam <kolinin@gmail.com>
2021-12-06 07:12:30 -0500

    comentarios controller, compilado perfil activado

--- reto2/backend/Retos_Ciclo4/src/main/java/Retos_Ciclo4/Retos_Ciclo4/controller/UserController.java
+++ reto2/backend/Retos_Ciclo4/src/main/java/Retos_Ciclo4/Retos_Ciclo4/controller/UserController.java
@@ -8,46 +8,88 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Clase controlador usuario
+ * @author camil
+ */
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin("*")
 public class UserController {
     @Autowired
+    /**
+     * servicio
+     */
     private UserService userService;
 
+    /**
+     * Metodo listar todos los usuarios
+     * @return lista
+     */
     @GetMapping("/all")
     public List<User> getAll() {
         return userService.getAll();
     }
-
+    
+    /**
+     * Metodo obtener un usuario
+     * @param id
+     * @return usuario
+     */
     @GetMapping("/{id}")
     public Optional<User> getUser(@PathVariable("id") int id) {
         return userService.getUser(id);
     }
-
+    
+    /**
+     * Metodo crear nuevo usuario
+     * @param user
+     * @return usuario
+     */
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User create(@RequestBody User user) {
         return userService.create(user);
     }
 
+    /**
+     * Metodo actualizar usuario
+     * @param user
+     * @return uuario
+     */
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
     public User update(@RequestBody User user) {
         return userService.update(user);
     }
-
+    
+    /**
+     * Metodo borrado de usuario
+     * @param id
+     * @return boolean
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public boolean delete(@PathVariable("id") int id) {
         return userService.delete(id);
     }
-
+    
+    /**
+     * Metodo autenticar usuario
+     * @param email
+     * @param password
+     * @return boolean
+     */
     @GetMapping("/{email}/{password}")
     public User authenticateUser(@PathVariable("email") String email, @PathVariable("password") String password) {
         return userService.authenticateUser(email, password);
     }
-
+    
+    /**
+     * Metodo verificar email usuario
+     * @param email
+     * @return boolean
+     */
     @GetMapping("/emailexist/{email}")
     public boolean emailExists(@PathVariable("email") String email) {
         return userService.emailExists(email);
